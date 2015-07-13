a09f6c3111f737fb9d56d658925e0fff5e29cb76
Neodiablow <francedupont6@hotmail.com>
2015-07-13 08:45:25 +0200

    initial commit

---
+++ files/includes.lua
@@ -0,0 +1,14 @@
+--General include file for My Awesome 
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+awful.rules = require("awful.rules")
+require("awful.autofocus")
+-- Widget and layout library
+local wibox = require("wibox")
+local vicious = require("vicious")
+-- Theme handling library
+local beautiful = require("beautiful")
+-- Notification library
+local naughty = require("naughty")
+local menubar = require("menubar")
